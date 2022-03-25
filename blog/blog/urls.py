707f80fbe37b0ca77135586099bd9736dfcbf8c1
purtuc.robert <purtuc.robert@gmail.com>
2022-03-25 10:32:43 +0200

    -Added photo upload -Added sitemap

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -1,27 +1,21 @@
-"""blog URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
 from django.urls import include
+from .sitemaps import CategorySitemap
+from .sitemaps import PostSitemap
 from core.views import front_page
 from core.views import about_page
+from django.conf.urls.static import static
+from django.conf import settings
+from django.contrib.sitemaps.views import sitemap
+
+sitemaps = {'category': CategorySitemap, 'post': PostSitemap}
 
 urlpatterns = [
+    path('sitemap.xml', sitemap, {'sitemaps': sitemaps},
+         name='django.contrib.sitemaps.views.sitemap'),
     path('admin/', admin.site.urls),
     path('about/', about_page),
     path('', include('blog_post.urls')),
     path('', front_page)
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
