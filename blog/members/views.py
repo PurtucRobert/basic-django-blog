2d0dbe92a3f0e7d8fa105915ff10ff6b82c3f1da
purtuc.robert <purtuc.robert@gmail.com>
2022-04-02 12:23:59 +0300

    Added the form validation into UI so now the user knows if the form was not completed sucessfully

--- blog/members/views.py
+++ blog/members/views.py
@@ -3,7 +3,6 @@ from django.shortcuts import render
 from django.shortcuts import redirect
 from django.contrib.auth import authenticate
 from django.contrib.auth import login
-from django.contrib.auth import logout
 from django.contrib import messages
 
 
